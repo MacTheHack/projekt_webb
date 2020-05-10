22a4825e0450219d320d1d38ac5aacba900d7de5
jonssonmattias <1jonssonmattias@gmail.com>
2020-05-10 15:28:10 +0200

    Made possible to translate text

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-console.log("main.js file is working");
+import $ from 'jquery';
+const API_URL = "https://api.funtranslations.com/translate/pirate.json?text=";
+
+
+function requestToAPI(text) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", API_URL + text, false );
+    xmlHttp.send( null );
+    var json = JSON.parse(xmlHttp.responseText);
+
+    return json.contents.translated;
+}
+
+$(document).ready(function() {
+    $("#convert").on("click", function(event) {
+        event.preventDefault();
+        var input = $("#inputText").val();
+        var translation = requestToAPI(input);
+        $("#translation").append(translation);
+    });
+});
