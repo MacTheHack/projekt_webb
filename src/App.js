b154d3b742904bede8180785e3aafd1ef2c9f610
MacTheHack <Markus.masalkovski@gmail.com>
2020-05-10 14:40:37 +0200

    Added subTexxt

--- src/App.js
+++ src/App.js
@@ -6,28 +6,35 @@ function Title(text){
   return (<h2 className="display-4">{text.welcomeText}</h2>);
 }
 
+function LeadText(lead){
+  return (<p className="lead">{lead.text}</p>);
+}
+
 function CreateForm(){
   return (
     <form className="text-left">
       <div className="form-group">
         <label for="inputText">Text to convert:</label>
         <textarea type="text" className="form-control" id="inputText" placeholder="Text to convert"></textarea>
+        <CreateButton id={"convert"} text={"Convert"}/>
+        <div id="translation"></div>
       </div>
     </form>
   );
 }
 
 function CreateButton(button){
-  return( <button className="btn btn-info btn-block" id={button.id}>{button.text}</button>)
+  return( <button className="btn btn-info btn-block mt-3" id={button.id}>{button.text}</button>)
 }
 
 function App() {
     return (
         <div className="App">
             <Title welcomeText={"Become a Pirate!"}/>
+            <LeadText text={"Take the first step to become a pirate.This webiste helps you to convert plain old boring English to cool pirate language"}/>
             <div className="container">
                 <CreateForm/>
-                <CreateButton id={"convert"} text={"Convert"}/>
+
             </div>
         </div>
 
